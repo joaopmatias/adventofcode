e3cc2512de4e1bbc9ed9ba0d9e7dac3932dc63b1
João Matias <joao.matias@talkdesk.com>
2022-12-03 13:48:47 +0000

    day 3 2022

---
+++ day3.py
@@ -0,0 +1,45 @@
+from io import StringIO
+
+import pandas as pd
+import numpy as np
+
+f = StringIO(
+"""
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""".strip()
+)
+f = open("day3.txt")
+total = 0
+for l in f:
+    ll = l.strip()
+    aa = set(ll[:len(ll) // 2])
+    bb = set(ll[len(ll) // 2:])
+    oo = ord((aa & bb).pop())
+    total += oo - 96 if oo > 95 else oo - 65 + 27
+
+print(f"The sum to priorities is {total}")
+
+f = StringIO(
+"""
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""".strip()
+)
+f = open("day3.txt")
+total = 0
+lines = f.readlines()
+for a, b, c in zip(lines[0::3], lines[1::3], lines[2::3]):
+    badge = (set(a.strip()) & set(b.strip()) & set(c.strip())).pop()
+    oo = ord(badge)
+    total += oo - 96 if oo > 95 else oo - 65 + 27
+    
+print(f"The sum to the badges priorities is {total}")
